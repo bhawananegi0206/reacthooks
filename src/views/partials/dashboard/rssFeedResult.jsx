0916e65005515a035bc2c79cf95739984295abc3
Bhawana negi <bhawananegi0224@gmail.com>
2020-07-06 12:21:05 +0200

    modified files

--- src/views/partials/dashboard/rssFeedResult.jsx
+++ src/views/partials/dashboard/rssFeedResult.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import PropTypes from 'prop-types'
-import simpleList from '../../../components/list/simpleList.jsx'
-
+import SimpleList from '../../../components/list/simpleList.jsx';
 import './rssFeedResult.scss'
-import SimpleList from '../../../components/list/simpleList.jsx'
+
 
 function rssFeedResult () {
   const feedData = useSelector(state => state.dashboard.feedData)
 
   const downloadableLinks = (items = []) =>
-    items.customfilter(item => item.links[0].url.indexOf('http') === 0)
+    items.filter(item => item.links[0].url.indexOf('http') === 0)
 
   const { items, title, description } = feedData
   const itemsList = downloadableLinks(items)
@@ -21,7 +20,7 @@ function rssFeedResult () {
         <div className='rssFeedResult'>
           <div className='rssFeedResult-listTitle center'>{title}</div>
           <div className='rssFeedResult-listDesc center'>{description}</div>
-          <simpleList items={itemsList} />
+          <SimpleList items={itemsList} />
         </div>
       )}
     </React.Fragment>
